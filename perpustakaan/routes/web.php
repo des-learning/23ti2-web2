c523c3b77d1923f000c7bbab318322aa724833ab
Des Dulianto <desdulianto@gmail.com>
2025-06-23 18:59:08 +0700

    add role to user
    admin page must authenticate first

--- perpustakaan/routes/web.php
+++ perpustakaan/routes/web.php
@@ -2,16 +2,21 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomepageController;
+use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomepageController::class, 'index']);
 Route::get('/books/{id}', [HomepageController::class, 'book']);
 
-Route::controller(AdminController::class)
-    ->prefix('admin')
-    ->name('admin.')
-    ->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/books', '...')->name('books');
-        Route::get('/categories', '...')->name('categories');
-});
+Route::middleware('auth')->group(
+    function() {
+        Route::controller(AdminController::class)
+            ->prefix('admin')
+            ->name('admin.')
+            ->group(function () {
+                Route::get('/', 'index')->name('index');
+                //Route::get('/books', '...')->name('books');
+                //Route::get('/categories', '...')->name('categories');
+        });
+    }
+);
