a86ec3e04c7ee7bca0f6b84584202fb174bfee97
Des Dulianto <desdulianto@gmail.com>
2025-06-23 18:19:40 +0700

    draft admin page

--- perpustakaan/routes/web.php
+++ perpustakaan/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomepageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomepageController::class, 'index']);
 Route::get('/books/{id}', [HomepageController::class, 'book']);
+
+Route::controller(AdminController::class)
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/books', '...')->name('books');
+        Route::get('/categories', '...')->name('categories');
+});
