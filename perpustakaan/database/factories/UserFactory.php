c4b3e65be086496f386ec4e672ce162d5b641fcc
Des Dulianto <desdulianto@gmail.com>
2025-07-03 09:37:04 +0700

    testing

--- perpustakaan/database/factories/UserFactory.php
+++ perpustakaan/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\UserRole;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -40,5 +41,14 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function adminRole(): static
+    {
+        return $this->state(
+            fn (array $attributes) => [
+                'role' => UserRole::Admin,
+            ],
+        );
     }
 }
--- perpustakaan/routes/web.php
+++ perpustakaan/routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomepageController;
-use Illuminate\Routing\RouteRegistrar;
 use Illuminate\Support\Facades\Route;
+
+require_once __DIR__ . '/my_routes.php';
 
 Route::get('/', [HomepageController::class, 'index']);
 Route::get('/books/{id}', [HomepageController::class, 'book']);
@@ -23,30 +22,3 @@ Route::middleware('auth')->group(
         adminRoutes();
     }
 );
-
-function adminRoutes(): RouteRegistrar
-{
-    return Route::controller(AdminController::class)
-            ->prefix('admin')
-            ->name('admin.')
-            ->group(function () {
-                Route::get('/', 'index')->name('index');
-                categoryAdminRoutes();
-        });
-}
-
-function categoryAdminRoutes(): RouteRegistrar
-{
-    return Route::controller(CategoryController::class)
-        ->prefix('category')
-        ->name('category.')
-        ->group(function() {
-            Route::get('/', 'index')->name('index');
-            Route::get('/create', 'create')->name('create');
-            Route::post('/create', 'store')->name('store');
-            Route::get('/{id}', 'show')->name('show');
-            Route::delete('/{id}', 'destroy')->name('destroy');
-            Route::get('/edit/{id}', 'edit')->name('edit');
-            Route::post('/edit/{id}', 'update')->name('update');
-        });
-}
